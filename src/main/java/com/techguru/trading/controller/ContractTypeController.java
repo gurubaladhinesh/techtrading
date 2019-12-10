958ad420ef9868b1e483438e3c4be38f49aa8250
gurubalad <gurubala.dhinesh@imaginea.com>
2019-12-10 07:54:31 +0530

    get operation controller

--- src/main/java/com/techguru/trading/controller/ContractTypeController.java
+++ src/main/java/com/techguru/trading/controller/ContractTypeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +34,7 @@ public class ContractTypeController {
 	}
 
 	@ApiOperation(value = "Finds all contract types", notes = "Finds all contract types")
-	@PostMapping
+	@GetMapping
 	public ResponseEntity<List<ContractType>> findAllContractTypes() {
 		return new ResponseEntity<>(contractTypeService.findAllContractTypes(), HttpStatus.OK);
 	}
