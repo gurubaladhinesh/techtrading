84febeeb4e36b5bbb63dc49cd044d61094932d8b
gurubalad <gurubala.dhinesh@imaginea.com>
2019-12-13 11:51:47 +0530

    deploy changes

--- src/main/java/com/techguru/trading/TechTradingApplication.java
+++ src/main/java/com/techguru/trading/TechTradingApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@Configuration
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @ComponentScan
 public class TechTradingApplication {
